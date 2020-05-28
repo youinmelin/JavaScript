65bd78cc2e1305bf93649462b4cc1248baab9a3b
youinme <ygqs@sina.com>
2020-05-28 17:20:00 +0800

    new

--- site/message_original.php
+++ site/message_original.php
@@ -11,9 +11,37 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>YOUINME</title>
-    <script src="jquery-1.12.2.min.js"></script>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/main.css">
+	<style type="text/css">
+        table
+        {
+            border-collapse: collapse;
+            margin: 0 auto;
+            text-align: center;
+        }
+        table td, table th
+        {
+            border: 1px solid #cad9ea;
+            color: #666;
+            height: 30px;
+        }
+        table thead th
+        {
+            background-color: #CCE8EB;
+            width: 200px;
+        }
+        table tr:nth-child(odd)
+        {
+            background: #fff;
+        }
+        table tr:nth-child(even)
+        {
+            background: #F5FAFA;
+        }
+		#dv{
+			background-color: white;
+		}
+    </style>
 </head>
 <body>
 <?php
@@ -51,33 +79,28 @@ if ($_POST){
 	看看大家伙都说啥了
 	<a href="">刷新</a>
 	<a href="index.php">首页</a>
-	<table class="table table-condensed">
-		<thead>
-		<th> 
-			 <a href="message.php?by=uname&order=desc">昵</a>
-			 <a href="message.php?by=uname&order=asc">称</a>  </th> 
-			 <th width = '70%'>留言</th> 
-			 <th ><a href="message.php?by=date&order=desc">时</a>
-			 <a href="message.php?by=date&order=asc">间</a> </th>
-		</thead>
-		<tbody>
-		<?php
-		if ($_GET){
-			$by = $_GET['by'];
-			$order = $_GET['order'];
-			$sql = "select uname,message,date,time from message order by $by $order;";
-		}else{
-			$sql = "select uname,message,date,time from message order by date desc;";
+	<table>
+	<thead>
+	<th>  <a href="message.php?by=uname&order=desc">昵</a><a href="message.php?by=uname&order=asc">称</a>  </th> <th width = '70%'>留言</th> <th ><a href="message.php?by=date&order=desc">时</a><a href="message.php?by=date&order=asc">间</a> </th
+	</thead>
+	<tbody>
+	<?php
+	if ($_GET){
+		$by = $_GET['by'];
+		$order = $_GET['order'];
+		$sql = "select uname,message,date,time from message order by $by $order;";
+	}else{
+		$sql = "select uname,message,date,time from message order by date desc;";
+	}
+	$ret = $conn->query($sql);	
+	if ($ret -> num_rows > 0){
+		while ( $row = $ret -> fetch_assoc()){
+			echo '<tr><th>'.$row['uname'].'</th><th width="60%">'.$row['message'].'</th><th>'.$row['date'].'</th></tr>';
 		}
-		$ret = $conn->query($sql);	
-		if ($ret -> num_rows > 0){
-			while ( $row = $ret -> fetch_assoc()){
-				echo '<tr><th>'.$row['uname'].'</th><th width="60%">'.$row['message'].'</th><th>'.$row['date'].'</th></tr>';
-			}
-		}
-		$conn->close();
-		?>
-		</tbody>
+	}
+	$conn->close();
+	?>
+	</tbody>
 	</table>
 </div>
 <script>
