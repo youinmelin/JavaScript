4242dc9afb811c70a199204cd473cb09990d2f95
youinmelin <ygqs@sina.com>
2020-10-08 22:15:05 +0800

    cms

--- vue.js/xc-ui-pc-teach/src/module/course/api/course.js
+++ vue.js/xc-ui-pc-teach/src/module/course/api/course.js
@@ -3,36 +3,46 @@ import querystring from 'querystring'
 let sysConfig = require('@/../config/sysConfig')
 let apiUrl = sysConfig.xcApiUrlPre;
 
-//查询课程列表
-//我的课程列表
+//查询课程列表 已连接后台
 export const findCourseList = (page,size,params) => {
 //使用工具类将json对象转成key/value
   let queries = querystring.stringify(params)
   return http.requestQuickGet(apiUrl+"/course/coursebase/list/"+page+"/"+size+"?"+queries)
 }
 
-//查询课程分类
+//查询课程分类 已连接后台
 export const category_findlist= () => {
   return http.requestQuickGet(apiUrl+'/category/list')
 }
-/*添加课程基础信息*/
+/*添加课程基础信息 已连接后台*/
 export const addCourseBase = params => {
   return http.requestPost(apiUrl+'/course/coursebase/add',params)
 }
-/*查询课程计划*/
+/*修改课程基础信息 已连接后台*/
+export const updateCoursebase = (courseid,params) => {
+  return http.requestPut(apiUrl+'/course/coursebase/update/'+ courseid,params)
+}
+
+/*查询课程计划 已连接后台*/
 export const findTeachplanList = courseid => {
   return http.requestQuickGet(apiUrl+'/course/teachplan/list/'+courseid)
 }
 
+/*查询课程基础信息 已连接后台*/
 export const getCoursebaseById = courseid => {
-  // return http.requestQuickGet(apiUrl+'/course/teachplan/list/'+courseid)
+  return http.requestQuickGet(apiUrl+'/course/coursebase/byid/'+courseid)
 }
-/*添加课程计划*/
+/*添加课程计划 已连接后台*/
 export const addTeachplan = teachplah => {
   return http.requestPost(apiUrl+'/course/teachplan/add',teachplah)
 }
 
-//保存课程图片地址到课程数据 库
+/*查询课程营销数据*/
+export const getCourseMarketById = courseid => {
+  return http.requestGet(apiUrl+'/course/coursemarket/get/'+courseid)
+}
+
+//保存课程图片地址到课程数据库
 export const addCoursePic= (courseId,pic) => {
   return http.requestPost(apiUrl+'/course/coursepic/add?courseId='+courseId+"&pic="+pic)
 }
